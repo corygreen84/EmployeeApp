7c575a994fccb9f1647d430f15bbc3cfeea783a6
Cory Green <corygreen84@hotmail.com>
2019-04-19 15:00:20 -0700

    c

--- functions/index.js
+++ functions/index.js
@@ -13,18 +13,59 @@ exports.updateUser = functions.firestore.document('companies/{company}/{jobs}/{j
     var job = context.params.job;
 
     var employeeBeforeChange = change.before.data();
-    for(var i in employeeBeforeChange.employees){
-        console.log("employees before " + employeeBeforeChange.employees[i]);
-    }
-
-    var employeeChange = change.after.data();
-    for(var key in employeeChange.employees){
-        console.log("employees after " + employeeChange.employees[key]);
-    }
-
-
-    // need to figure out what changed.  
+    var employeeAfterChange = change.after.data();
     
+    var returnedData = checkDifferenceBetweenTwoArrays(employeeBeforeChange, employeeAfterChange);
 
-
+    for(var keys in returnedData){
+        console.log(returnedData[keys]);
+    }
 });
+
+
+
+function checkDifferenceBetweenTwoArrays(_originalArray, _updatedArray){
+
+	let tempArrayOfOriginalEmailsToDelete = _originalArray;
+	let tempArrayOfUpdatedEmailsToAdd = _updatedArray;
+	let tempArrayOfSameEmails = [];
+
+	let returnDictionary = {};
+
+	
+	// going through and seeing what is the same out of the updated array compared to the original //
+	for(var original in _originalArray){
+		for(var updated in _updatedArray){
+			if(_originalArray[original] === _updatedArray[updated]){
+				tempArrayOfSameEmails.push(_originalArray[original]);
+			}
+		}
+	}
+
+	// going through the original array and getting whats already in the updated array //
+	// this is the to be deleted list of emails //
+	for(var k in tempArrayOfOriginalEmailsToDelete){
+		for(var l in tempArrayOfSameEmails){
+			if(tempArrayOfOriginalEmailsToDelete[k] === tempArrayOfSameEmails[l]){
+                //tempArrayOfOriginalEmailsToDelete.splice(k, 1);
+                delete tempArrayOfOriginalEmailsToDelete[k];
+			}
+		}
+	}
+
+	for(var m in tempArrayOfSameEmails){
+		for(var n in tempArrayOfUpdatedEmailsToAdd){
+			if(tempArrayOfUpdatedEmailsToAdd[n] === tempArrayOfSameEmails[m]){
+                //tempArrayOfUpdatedEmailsToAdd.splice(n, 1);
+                delete tempArrayOfUpdatedEmailsToAdd[k];
+			}
+		}
+	}
+
+	returnDictionary["delete"] = tempArrayOfOriginalEmailsToDelete;
+	returnDictionary["add"] = tempArrayOfUpdatedEmailsToAdd;
+	returnDictionary["same"] = tempArrayOfSameEmails;
+
+	return returnDictionary;
+	
+}
