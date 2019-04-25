e25a1dfc1d35827f0dc14f79bf8d8361e0230fbd
Cory Green <corygreen84@hotmail.com>
2019-04-25 14:52:09 -0700

    got the cloud functions working for creating a new job modifying a job and deleting a job

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,52 @@
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
 
 // **** when we get a write to the employees section of the job, we need to also update the employee with the job data **** //
 // **** user modifies the employees on the job, the job then notifies the employees as to what job they have **** //
 exports.updateUser = functions.firestore.document('companies/{company}/{jobs}/{job}').onUpdate((change, context) =>{
-    //var email = change.after.data().email;
-    
+
     var company = context.params.company;
-    var job = context.params.job;
+	var job = context.params.job;
 
     var employeeBeforeChange = change.before.data();
     var employeeAfterChange = change.after.data();
-    
-    var returnedData = checkDifferenceBetweenTwoArrays(employeeBeforeChange, employeeAfterChange);
+	
+	var db = admin.firestore();
+	var returnedData = checkDifferenceBetweenTwoArrays(employeeBeforeChange["employees"], employeeAfterChange["employees"]);
+	
+	var deletedEmployees = returnedData["delete"];
+	var addedEmployees = returnedData["add"];
 
-    for(var keys in returnedData){
-        console.log(returnedData[keys]);
-    }
+	
+
+	// if the job has been deleted, then I need to take the job away from the employee //
+
+
+	// going through and removing the employees //
+	for(var i in deletedEmployees){
+		var dbD = db.doc('companies/' + company + '/employees/' + deletedEmployees[i]);
+
+		dbD.update({
+			jobs: admin.firestore.FieldValue.arrayRemove(job)
+		});
+	}
+
+
+
+	// going through and adding the employees //
+	for(var j in addedEmployees){
+		var dbDoc = db.doc('companies/' + company + '/employees/' + addedEmployees[j]);
+		
+
+		dbDoc.update({
+			jobs: admin.firestore.FieldValue.arrayUnion(job)
+		});
+	}
+
+	return 0;
 });
 
 
@@ -47,7 +74,6 @@ function checkDifferenceBetweenTwoArrays(_originalArray, _updatedArray){
 	for(var k in tempArrayOfOriginalEmailsToDelete){
 		for(var l in tempArrayOfSameEmails){
 			if(tempArrayOfOriginalEmailsToDelete[k] === tempArrayOfSameEmails[l]){
-                //tempArrayOfOriginalEmailsToDelete.splice(k, 1);
                 delete tempArrayOfOriginalEmailsToDelete[k];
 			}
 		}
@@ -56,8 +82,7 @@ function checkDifferenceBetweenTwoArrays(_originalArray, _updatedArray){
 	for(var m in tempArrayOfSameEmails){
 		for(var n in tempArrayOfUpdatedEmailsToAdd){
 			if(tempArrayOfUpdatedEmailsToAdd[n] === tempArrayOfSameEmails[m]){
-                //tempArrayOfUpdatedEmailsToAdd.splice(n, 1);
-                delete tempArrayOfUpdatedEmailsToAdd[k];
+                delete tempArrayOfUpdatedEmailsToAdd[n];
 			}
 		}
 	}
@@ -69,3 +94,52 @@ function checkDifferenceBetweenTwoArrays(_originalArray, _updatedArray){
 	return returnDictionary;
 	
 }
+
+
+
+
+exports.deleteUser = functions.firestore.document('companies/{company}/{jobs}/{job}').onDelete((change, context) =>{
+
+	const deletedValue = change.data();
+	var db = admin.firestore();
+
+	var company = context.params.company;
+	var job = context.params.job;
+
+	var employees = deletedValue["employees"];
+	
+
+	for(var k in employees){
+		var dbD = db.doc('companies/' + company + '/employees/' + employees[k]);
+
+		dbD.update({
+			jobs: admin.firestore.FieldValue.arrayRemove(job)
+		});
+	}
+
+	return 0;
+});
+
+
+
+
+exports.createUser = functions.firestore.document('companies/{company}/{jobs}/{job}').onCreate((change, context) =>{
+
+	var db = admin.firestore();
+	var company = context.params.company;
+	var job = context.params.job;
+
+	const addedValue = change.data();
+
+	var employees = addedValue["employees"];
+
+	for(var k in employees){
+		var dbD = db.doc('companies/' + company + '/employees/' + employees[k]);
+
+		dbD.update({
+			jobs: admin.firestore.FieldValue.arrayUnion(job)
+		});
+	}
+
+	return 0;
+});
