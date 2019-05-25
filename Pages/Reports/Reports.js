76b918964ffffaf3b5858cb96af5b69ec84d4f93
Cory Green <corygreen84@hotmail.com>
2019-05-25 02:35:48 -0700

    c

--- Pages/Reports/Reports.js
+++ Pages/Reports/Reports.js
@@ -93,13 +93,16 @@ function parseEmployeeData(employees){
 
 		var employeeListUl = $("#employee-list-ul");
 		var employeeListItem = '<li id=employee-Report-' + e + '><div data-role="collapsible"><h2>' + name + 
-		'</h2><ul data-role="listview" data-theme="a" data-divider-theme="b">';
+		'</h2>';
+
+		var employeeDataUl = '<ul data-role="listview" data-theme="a" data-divider-theme="b">';
 
 		employeeListUl.append(employeeListItem);
+		employeeListItem.append(employeeDataUl);
 
 		var listItem = $('<li><a href="index.html"><h3>Stephen Weber</h3><p><strong>Youve been invited to a meeting at Filament Group in Boston, MA</strong></p><p>Hey Stephen, if youre available at 10am tomorrow, weve got a meeting with the jQuery team.</p><p class="ui-li-aside"><strong>6:24</strong>PM</p></a></li>');
 
-		employeeListItem.append(listItem);
+		employeeDataUl.append(listItem);
 
 
 		
