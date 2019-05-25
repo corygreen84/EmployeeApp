1fc9efd38bb573454275073dfba1c5cc002ae389
Cory Green <corygreen84@hotmail.com>
2019-05-25 13:58:47 -0700

    c

--- Pages/Reports/Reports.js
+++ Pages/Reports/Reports.js
@@ -89,10 +89,6 @@ function parseEmployeeData(employees){
 		
 		//console.log(htmlEventString);
 
-
-
-		
-		
 		$("#employee-list-ul").append('<li><h2>'+ name + 
 		'</h2><ul data-role="listview" data-theme="a" data-divider-theme="b"><li><a href="index.html"><h3>Stephen Weber</h3><p><strong>Youve been invited to a meeting at Filament Group in Boston, MA</strong></p><p>Hey Stephen, if youre available at 10am tomorrow, weve got a meeting with the jQuery team.</p><p class="ui-li-aside"><strong>6:24</strong>PM</p></a></li></ul></li>');
 		
