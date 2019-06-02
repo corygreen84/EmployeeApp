bdc7ad55da9f545a272c02f3f06c958c54c61f31
Cory Green <corygreen84@hotmail.com>
2019-06-02 00:46:44 -0700

    c

--- functions/index.js
+++ functions/index.js
@@ -4,4 +4,6 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 
-
+exports.scheduledFunction = functions.pubsub.schedule('2 * * * *').timeZone('America/Los_Angles').onRun((context) => {
+    console.log("Hey there");
+});
