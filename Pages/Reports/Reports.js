09c61613c8f01b672e8d99cf49d0885c80786d30
Cory Green <corygreen84@hotmail.com>
2019-06-06 14:15:39 -0700

    c

--- Pages/Reports/Reports.js
+++ Pages/Reports/Reports.js
@@ -90,6 +90,7 @@ function parseEmployeeData(employees){
 
 		// employee history has changed.  It is now just a data string that I need to pull appart //
 		var employeeHistory = employees[e].jobHistory;
+		console.log(employeeHistory);
 		var jsonData = JSON.parse(employeeHistory);
 		console.log(jsonData);
 
