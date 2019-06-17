679240aaecc86caa7fd0aaf401740be5cf8f21b7
Cory Green <corygreen84@hotmail.com>
2019-06-17 09:48:56 -0700

    c

--- Pages/Modify Job/SearchForJob.js
+++ Pages/Modify Job/SearchForJob.js
@@ -6,7 +6,10 @@ function searchForJob(job, company){
 	var jobRef = db.collection('companies').doc(company).collection('jobs').doc(job);
 	jobRef.get().then(function(doc){
 		if(doc.exists){
-			console.log("data -> " + doc.data());
+			var data = doc.data();
+			for(var i in data){
+				console.log("data -> " + data[i]);
+			}
 		}else{
 
 		}
