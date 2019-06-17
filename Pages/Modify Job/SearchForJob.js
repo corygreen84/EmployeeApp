1198c4afd9317aa9f202a6b0f409024392ce3179
Cory Green <corygreen84@hotmail.com>
2019-06-17 13:23:19 -0700

    c

--- Pages/Modify Job/SearchForJob.js
+++ Pages/Modify Job/SearchForJob.js
@@ -50,41 +50,34 @@ function searchForJob(job, company){
 }
 
 function loadIntoFields(job){
-	modifyJobNameTextField.value = job.name;
-	modifyJobAddressTextField.value = job.address;
-	modifyJobLongitudeTextField.value = "" + job.long;
-	modifyJobLatitudeTextField.value = "" + job.lat;
-	if(job.notes != undefined){
-		modifyJobNotes.value = job.notes;
-	}
 
+	setThingsToDefault(job.name, job.address, job.long, job.lat, job.notes);
 
-	// this is the opposite of what we think //
-	// I want the search and modify buttons to be blanked out when the fields are first loaded //
-	modifyNameFilledIn = false;
-	modifyAddressFilledIn = false;
-	modifyLongFilledIn = false;
-	modifyLatFilledIn = false;
-	modifyNotesFilledIn = false;
-
-	toggleSearchButton();
-
-	putOnMap(job.address, job.long, job.lat);
 }
 
-function putOnMap(address, long, lat){
-	placeOnMapModify(long, lat, address);
-}
+
+
+
+
 
 
 
 function clearButtonClicked(){
-	modifyJobNameTextField.value = modifiedName
-	modifyJobAddressTextField.value = modifiedAddress;
-	modifyJobLongitudeTextField.value = modifiedLocation["longitude"];
-	modifyJobLatitudeTextField.value = modifiedLocation["latitude"];
+
+	setThingsToDefault(modifiedName, modifiedAddress, modifiedLocation["longitude"], modifiedLocation["latitude"], modifiedNotes);
+
+	// getting the default employee data //
+	loadEmployeesModify(passedInCompany, jobLoaded.employees);
+}
+
+
+function setThingsToDefault(name, address, long, lat, notes){
+	modifyJobNameTextField.value = name
+	modifyJobAddressTextField.value = address;
+	modifyJobLongitudeTextField.value = long;
+	modifyJobLatitudeTextField.value = lat;
 	if(modifiedNotes != undefined){
-		modifyJobNotes.value = modifiedNotes;
+		modifyJobNotes.value = notes;
 	}else{
 		modifyJobNotes.value = "";
 	}
@@ -97,8 +90,9 @@ function clearButtonClicked(){
 
 	toggleSearchButton();
 
-	putOnMap(modifiedAddress, modifiedLocation["longitude"], modifiedLocation["latitude"]);
+	putOnMap(address, long, lat);
+}
 
-	// getting the default employee data //
-	loadEmployeesModify(passedInCompany, jobLoaded.employees);
+function putOnMap(address, long, lat){
+	placeOnMapModify(long, lat, address);
 }
