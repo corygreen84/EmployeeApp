71a9c3c3bdddffc2fe4a529fdef0d9bec9ad4b60
Cory Green <corygreen84@hotmail.com>
2019-06-25 22:01:23 -0700

    c

--- Pages/Reports/Reports.js
+++ Pages/Reports/Reports.js
@@ -104,11 +104,13 @@ function parseEmployeeData(employees){
 
 		if(employees[e].jobHistory != "" && employees[e].jobHistory != undefined){
 
+			console.log(employees[e].jobHistory);
+
 			//var convertedString = convertStringToJSONData("{" + employees[e].jobHistory + "}");
 			var convertedString = convertStringToJSONData(employees[e].jobHistory);
 			var jsonData = JSON.parse(convertedString);
 
-			console.log(jsonData);
+			//console.log(jsonData);
 
 			for(var i in jsonData){
 
